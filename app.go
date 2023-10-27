b876282ead6ca2e71bd7b5b93fc7d426e800fe3e
denysov.kos <denysov.kos@gmail.com>
2023-10-27 13:37:50 +0200

    Refactored to gin

--- app.go
+++ app.go
@@ -1,32 +1,11 @@
 package main
 
 import (
-	"embed"
-	"html/template"
-    	"net/http"
-	"os"
+	"github.com/gin-gonic/gin"
 )
 
-//go:embed templates/*
-var templates embed.FS
-var t = template.Must(template.ParseFS(templates, "templates/*"))
-
-//go:embed static/*
-var assets embed.FS
-
-
 func main() {
-	port := "3000"
-	if(os.Getenv("PORT") != "") {
-		port = os.Getenv("PORT")
-	}
-
-    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
-        t.Execute(w, "")
-    })
-
-    fs := http.FileServer(http.FS(assets))
-    http.Handle("/static/", fs)
-
-    http.ListenAndServe(":" + port, nil)
+	r := gin.Default()
+	r.Static("/", "./static")
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
