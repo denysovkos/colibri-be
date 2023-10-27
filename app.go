9dac25e61b40a4afe4fca0b7873e8e9cc32d6e34
denysov.kos <denysov.kos@gmail.com>
2023-10-27 13:49:38 +0200

    added static bundler

--- app.go
+++ app.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"embed"
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
+
+var static embed.FS
 
 func main() {
 	r := gin.Default()
 	r.Static("/", "./static")
+	http.Handle("/static", http.FileServer(http.FS(static)))
 	r.Run() // listen and serve on 0.0.0.0:8080
 }
